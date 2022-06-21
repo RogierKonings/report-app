dab5dd21fd7ef02d489b96729dbf2c8a12f5ecdf
Rogier Konings <r.konings@portbase.com>
2022-06-21 20:42:24 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: async () =>
+      (await import('src/app/pages/report-page/report-page.routing-module'))
+        .ReportPageRoutingModule,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
