d04fe11a2aff7d0359e31fbaf7391bfc24f5e40a
Rogier Konings <rogier.konings@randstadgroep.nl>
2023-05-12 12:14:52 +0200

    add new prettier rules and format

--- src/app/pages/report-page/report-page.component.spec.ts
+++ src/app/pages/report-page/report-page.component.spec.ts
@@ -1,9 +1,9 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ReportPageComponent } from './report-page.component';
-import { ReportService } from '../../services/report.service';
-import { of } from 'rxjs';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing'
+import {By} from '@angular/platform-browser'
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core'
+import {ReportPageComponent} from './report-page.component'
+import {ReportService} from '../../services/report.service'
+import {of} from 'rxjs'
 
 class ReportServiceMock {
   createReport() {
@@ -12,33 +12,33 @@ class ReportServiceMock {
 }
 
 describe('ReportPageComponent', () => {
-  let component: ReportPageComponent;
-  let fixture: ComponentFixture<ReportPageComponent>;
-  let reportService: ReportService;
+  let component: ReportPageComponent
+  let fixture: ComponentFixture<ReportPageComponent>
+  let reportService: ReportService
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ReportPageComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      providers: [{ provide: ReportService, useClass: ReportServiceMock }],
-    }).compileComponents();
-  }));
+      providers: [{provide: ReportService, useClass: ReportServiceMock}],
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ReportPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-    reportService = TestBed.inject(ReportService);
-  });
+    fixture = TestBed.createComponent(ReportPageComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+    reportService = TestBed.inject(ReportService)
+  })
 
   describe('when opening a file', () => {
     it('should call the create report method from the report service', waitForAsync(() => {
-      const blob = new Blob([''], { type: 'text/csv' });
-      const fakeFile = blob as File;
+      const blob = new Blob([''], {type: 'text/csv'})
+      const fakeFile = blob as File
 
-      jest.spyOn(reportService, 'createReport');
-      component.openFile(fakeFile);
+      jest.spyOn(reportService, 'createReport')
+      component.openFile(fakeFile)
       expect(reportService.createReport).toHaveBeenCalled()
-    }));
-  });
-});
+    }))
+  })
+})
